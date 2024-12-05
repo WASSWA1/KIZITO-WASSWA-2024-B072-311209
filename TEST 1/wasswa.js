759220087c4996fa05cfd6e4514ef797cb03739f
WASSWA1 <wasswakizito843@gmail.com>
2024-12-05 02:51:49 -0800

    Add files via upload

---
+++ TEST 1/wasswa.js
@@ -0,0 +1,10 @@
+function wasswa(){
+	alert("how can i help you");
+	alert("whats your name");
+	alert("whats ur course");
+	alert("whats your favourate course unit");
+}
+function wasswa2(){
+	alert("do you now kizito wasswa");
+	alert("what do you now from him");
+	alert("oky thank you for you massage");
